1abc5f91d9b2a87869c7b590eb69c4f869cb37cc
Ailwei <Ailweimaemu@gmail.com>
2024-02-24 01:05:33 -0800

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,9 @@ from flask import Blueprint
 from api.v1.views import app_views
 from werkzeug.exceptions import NotFound
 from flask_cors import CORS
+"""
+error page
+"""
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
@@ -20,7 +23,7 @@ def handle_not_found_error(e):
     return response
 
 
-if __name__ == "__main__":
+if '__name__' == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
