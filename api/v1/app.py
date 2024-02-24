63ec9bc321f34f491faf4f824880a45f97eedf0e
Ailwei <Ailweimaemu@gmail.com>
2024-02-24 01:03:51 -0800

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,9 +8,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
 
 def handle_not_found_error(e):
     response = jsonify({"error": "Not found"})
@@ -22,5 +24,3 @@ if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
-
-
