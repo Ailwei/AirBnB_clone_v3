ab398e866d5831ed4e1ff60df6d940f964bd7fd5
Ailwei <Ailweimaemu@gmail.com>
2024-02-25 00:24:31 -0800

    pdate

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,14 +6,14 @@ index file
 import sys
 sys.path.append('/AirBnB_clone_v3')
 from flask import jsonify
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.user import User
-from models.state import State
+#from models.city import City
+#from models.place import Place
+#from models.review import Review
+#from models.user import User
+#from models.state import State
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
+#from models.amenity import Amenity
 
 
 @app_views.route('/status', methods=['GET'])
