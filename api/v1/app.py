eeb84bdb8d0799cb569bb9fa03a0501f5a75dedd
Ailwei <Ailweimaemu@gmail.com>
2024-02-25 06:43:34 -0800

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,7 +15,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
