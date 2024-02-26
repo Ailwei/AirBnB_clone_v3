5a61bf11c54bbe737fbbfe5bea3aad594d1b0e11
Ailwei <Ailweimaemu@gmail.com>
2024-02-26 06:20:59 -0800

    update model

--- models/city.py
+++ models/city.py
@@ -14,8 +14,11 @@ class City(BaseModel, Base):
     if models.storage_t == "db":
         __tablename__ = 'cities'
         id = Column(String(60), primary_key=True, nullable=False)
-        created_at =Column(DateTime, nullable=False, default=datetime.utcnow)
-        updated_at = Column(DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+        created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+        updated_at = Column(
+                DateTime, nullable=False,
+                default=datetime.utcnow, onupdate=datetime.utcnow
+                )
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
         places = relationship("Place", backref="cities")
