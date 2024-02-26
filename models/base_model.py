e1079671e2bd8df3a32b9180e0afaa211a7485c4
Ailwei <Ailweimaemu@gmail.com>
2024-02-26 03:33:17 -0800

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -60,16 +60,15 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
-        new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-        return new_dict
+        """Return a dictory represantion of the BaseModel instance"""
+        base_dict = {
+                'id': self.id,
+                'created_at': self.created_at.isoformat(),
+                'updated_at': self.updated_at.isoformat(),
+                }
+        if not exclude_password:
+            base_dict['password'] = self.password
+        return base_dict
 
     def delete(self):
         """delete the current instance from the storage"""
